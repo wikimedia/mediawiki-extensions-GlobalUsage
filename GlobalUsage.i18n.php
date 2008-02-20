eb27e15f887860e4afe4b900b562d72a54ad42f2
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-02-20 15:37:10 +0000

    Localisation updates for extension messages from Betawiki (2008-02-10 16:10 CET)

--- GlobalUsage.i18n.php
+++ GlobalUsage.i18n.php
@@ -13,3 +13,25 @@ $messages = array();
 $messages['en'] = array(
 	'globalusage-desc' => '[[Special:GlobalUsage|Special page]] to view global file usage',
 );
+
+/** French (Français)
+ * @author Grondin
+ */
+$messages['fr'] = array(
+	'globalusage-desc' => '[[Special:GlobalUsage|Page spéciale]] pour voir l’usage global d’une image',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'globalusage-desc' => '[[Special:GlobalUsage|Specialna strona]], zo by globalne wužiwanje wobraza widźał',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'globalusage-desc' => '[[Special:GlobalUsage|Speciale pagina]] voor het bekijken van globaal bestandsgebruik',
+);
+
