db95ba4f3c9deadca3d063586f2846741857cfa3
Raimond Spekking <raymond@users.mediawiki.org>
2009-12-19 21:50:13 +0000

    Localisation updates for core and extension messages from translatewiki.net (2009-12-18 21:00 UTC)

--- GlobalUsage.i18n.php
+++ GlobalUsage.i18n.php
@@ -25,29 +25,30 @@ $messages['en'] = array(
 
 /** Message documentation (Message documentation)
  * @author Jon Harald Søby
+ * @author Nghtwlkr
  * @author Purodha
  * @author Raymond
  * @author Umherirrender
  * @author Vriullop
  */
 $messages['qqq'] = array(
-	'globalusage-for' => 'Title of [[Special:GlobalUsage]]
-* $1 - name of the searched file, with namespace',
+	'globalusage-for' => 'Tittel på [[Special:GlobalUsage]]
+* $1 - navn på den ettersøkte filen, med navnerom',
 	'globalusage-desc' => 'Short description of this extension, shown on [[Special:Version]]. Do not translate or change links.',
 	'globalusage-ok' => '{{Identical|Search}}
 Button on [[Special:GlobalUsage]]',
 	'globalusage-text' => 'Label on [[Special:GlobalUsage]]',
-	'globalusage-no-results' => 'Used for empty result on [[Special:GlobalUsage]]
-* $1 - name of the file (with namespace)',
-	'globalusage-on-wiki' => 'Shown in the list of global usages. Example: [[Commons:Special:GlobalUsage/Example.jpg]] and [[Commons:File:Example.jpg]]
+	'globalusage-no-results' => 'Brukt for tomme resultat på [[Special:GlobalUsage]]
+* $1 - navn på filen (med navnerom)',
+	'globalusage-on-wiki' => 'Vist i listen over global bruk. Eksempel: [[Commons:Special:GlobalUsage/Example.jpg]] og [[Commons:File:Example.jpg]]
 
-* $1 the file name, but unused in this message
-* $2 the project name',
-	'globalusage-of-file' => 'Used on a image page.
-The number of following wikis is unknown. By a empty result, no message is shown.',
-	'globalusage-more' => 'Used on an image page, when more global usage results are available. Example: [[Commons:File:Example.jpg]]
-* $1 - name of the file (without namespace)',
-	'globalusage-filterlocal' => 'Filteroption for [[Special:GlobalUsage]]',
+* $1 filnavnet, men ikke brukt i denne meldingen
+* $2 prosjektnavnet',
+	'globalusage-of-file' => 'Brukt på en bildeside.
+Antallet følgende wikier er ukjent. Ved tomt resultat vises ingen melding.',
+	'globalusage-more' => 'Brukt på en bildeside når flere globale bruksresultat er tilgjengelige. Eksempel: [[Commons:File:Example.jpg]]
+* $1 - navn på filen (uten navnerom)',
+	'globalusage-filterlocal' => 'Filtervalg for [[Special:GlobalUsage]]',
 );
 
 /** Karelian (Karjala)
@@ -823,12 +824,19 @@ $messages['nn'] = array(
 
 /** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
  * @author Jon Harald Søby
+ * @author Nghtwlkr
  */
 $messages['no'] = array(
 	'globalusage' => 'Global filbruk',
+	'globalusage-for' => 'Global bruk av "$1"',
 	'globalusage-desc' => '[[Special:GlobalUsage|Spesialside]] for å vise bruken av en fil globalt',
 	'globalusage-ok' => 'Søk',
 	'globalusage-text' => 'Søk global filbruk.',
+	'globalusage-no-results' => '[[:$1]] er ikke brukt på andre wikier.',
+	'globalusage-on-wiki' => 'Bruk i $2',
+	'globalusage-of-file' => 'Følgende andre wikier bruker denne filen:',
+	'globalusage-more' => 'Vis [[{{#Special:GlobalUsage}}/$1|mer global bruk]] av denne filen',
+	'globalusage-filterlocal' => 'Ikke vis lokal bruk',
 );
 
 /** Occitan (Occitan)
@@ -840,7 +848,11 @@ $messages['oc'] = array(
 	'globalusage-desc' => '[[Special:GlobalUsage|Pagina especiala]] per veire l’usatge global d’un imatge',
 	'globalusage-ok' => 'Recèrca',
 	'globalusage-text' => "Recercar l'usatge global del fichièr",
-	'globalusage-on-wiki' => 'Utilizacion de [[:File:$1|$1]] sus $2',
+	'globalusage-no-results' => "[[:$1]] es pas utilizat sus d'autres wikis.",
+	'globalusage-on-wiki' => 'Utilizacion sus $2',
+	'globalusage-of-file' => 'Los autres wikis seguents utilizan aqueste imatge :',
+	'globalusage-more' => "Veire [[{{#Special:GlobalUsage}}/$1|d'autras utilizacions globalas]] d'aqueste fichièr.",
+	'globalusage-filterlocal' => "Far pas veire l'utilizacion locala",
 );
 
 /** Oriya (ଓଡ଼ିଆ)
