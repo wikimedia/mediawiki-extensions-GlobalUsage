f5085b3c2b9b829c05a2fd613a2f0f35908121d0
Alexander Vorwerk <zabe@avorwerk.net>
2025-08-26 14:27:56 +0200

    Drop support for categorylinks read old
    Bug: T299951
    Change-Id: I6e9c7c853dd37e2a92caab2952ffe4909552e964

--- includes/GlobalUsageQuery.php
+++ includes/GlobalUsageQuery.php
@@ -2,7 +2,6 @@
 
 namespace MediaWiki\Extension\GlobalUsage;
 
-use MediaWiki\MainConfigNames;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\Title\Title;
 use MediaWiki\WikiMap\WikiMap;
@@ -199,24 +198,12 @@ class GlobalUsageQuery {
 			case NS_CATEGORY:
 				$queryBuilder->join( 'categorylinks', null, 'page_id = cl_from' );
 				$queryBuilder->join( 'page', null, 'page_title = gil_to' );
-
-				$migrationStage = MediaWikiServices::getInstance()->getMainConfig()->get(
-					MainConfigNames::CategoryLinksSchemaMigrationStage
-				);
-
-				if ( $migrationStage & SCHEMA_COMPAT_READ_OLD ) {
-					$queryBuilder->where( [
-						'cl_to' => $queryIn,
-						'page_namespace' => NS_FILE,
-					] );
-				} else {
-					$queryBuilder->join( 'linktarget', null, 'lt_id = cl_target_id' );
-					$queryBuilder->where( [
-						'lt_title' => $queryIn,
-						'lt_namespace' => NS_CATEGORY,
-						'page_namespace' => NS_FILE,
-					] );
-				}
+				$queryBuilder->join( 'linktarget', null, 'lt_id = cl_target_id' );
+				$queryBuilder->where( [
+					'lt_title' => $queryIn,
+					'lt_namespace' => NS_CATEGORY,
+					'page_namespace' => NS_FILE,
+				] );
 				break;
 			default:
 				return;
